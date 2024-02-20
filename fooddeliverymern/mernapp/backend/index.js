871adab78e241eb33d0f041891082d408a05459c
Saikat Saha <saikats_5@yahoo.co.in>
2024-02-20 22:38:34 +0530

    Route added with mongoose configuration

--- fooddeliverymern/mernapp/backend/index.js
+++ fooddeliverymern/mernapp/backend/index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5500
 const mongoDB = require("./db");
-mongoDB(); 
+mongoDB();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.use(express.json());
+app.use('/api', require('./Routes/CreateUser'));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
