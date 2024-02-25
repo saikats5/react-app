bec2b0cb38f16a75538112c06aee5b578bfba40a
Saikat Saha <saikats_5@yahoo.co.in>
2024-02-25 14:52:00 +0530

    Code optimized

--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -1,21 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const handleGetPostsClick = () => {
+  const [dataFetched, setDataFetched] = useState(false);
+  if (!dataFetched) {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-  };
+    setDataFetched(true);
+  }
+
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
