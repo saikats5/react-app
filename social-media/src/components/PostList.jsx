3b59f3967aab4dacb1103b51e2bbf1fdb9b2e5b1
Saikat Saha <saikats_5@yahoo.co.in>
2024-02-25 18:29:30 +0530

    call abort concept added

--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -8,12 +8,18 @@ const PostList = () => {
   const [fetching, setFetching] = useState(false);
   useEffect(() => {
     setFetching(true);
-    fetch("https://dummyjson.com/posts")
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch("https://dummyjson.com/posts", { signal })
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
         setFetching(false);
       });
+    return () => {
+      console.log("Cleaning up useEffect - Unmount");
+      controller.abort();
+    };
   }, []);
 
   return (
