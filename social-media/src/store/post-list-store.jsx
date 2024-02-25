6f5276ac01204fdb52828cc67de61e97ff80b1e4
Saikat Saha <saikats_5@yahoo.co.in>
2024-02-25 11:11:46 +0530

    post code modified

--- social-media/src/store/post-list-store.jsx
+++ social-media/src/store/post-list-store.jsx
@@ -1,19 +1,18 @@
 import { createContext, useReducer } from "react";
-const DEFAULT_CONTEXT = {
+
+export const PostList = createContext({
   postList: [],
   addPost: () => {},
   deletePost: () => {},
-};
-const PostList = createContext(DEFAULT_CONTEXT);
+});
 const postListReducer = (currPostList, action) => {
   return currPostList;
 };
 const PostListProvider = ({ children }) => {
-  const [postList, dispatchPostList] = useReducer(postListReducer, {
-    postList: [],
-    addPost,
-    deletePost,
-  });
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
   const addPost = () => {};
   const deletePost = () => {};
   return (
@@ -22,4 +21,22 @@ const PostListProvider = ({ children }) => {
     </PostList.Provider>
   );
 };
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Test1",
+    body: "Test1 Body",
+    reactions: 11,
+    userId: "user-1",
+    tags: ["111", "222", "333"],
+  },
+  {
+    id: "2",
+    title: "Test2",
+    body: "Test2 Body",
+    reactions: 22,
+    userId: "user-2",
+    tags: ["444", "555", "666"],
+  },
+];
 export default PostListProvider;
