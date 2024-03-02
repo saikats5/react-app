88a3b3b6c59dc040f27b511a62173ff9249883c1
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-02 11:09:51 +0530

    new code added

--- reacttask/react-task/src/components/InputField.tsx
+++ reacttask/react-task/src/components/InputField.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import './styles.css'
 
-interface Props {}
+interface Props {
+  todo: string
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
 
-const InputField = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
       <input
         type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
         placeholder="Enter a task"
         className="input__box"
       ></input>
