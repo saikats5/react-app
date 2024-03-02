9420f3193745155b0dc39e145c02738052b8cd86
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-02 18:11:16 +0530

    base code added

--- ecommerce/frontend/src/App.tsx
+++ ecommerce/frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   return (
     <div>
       <header></header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer></footer>
     </div>
   )
