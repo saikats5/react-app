688e7ae8b262c2d4919110915aefb1770e9e819f
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-14 10:43:03 +0530

    Router added

--- react-concepts/advanced-concept/src/App.js
+++ react-concepts/advanced-concept/src/App.js
@@ -1,16 +1,25 @@
 import './App.css'
-//import About from './components/About'
+import About from './components/About'
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Navbar title={'Main Utils'} />
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyse" />
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar title={'Main Utils'} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route
+              exact
+              path="/"
+              element={<TextForm heading="Enter the text to analyse" />}
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
