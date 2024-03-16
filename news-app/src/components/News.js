2ba7acdc71676c5caeee47db1d9fc0c50901eebe
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-16 13:49:03 +0530

    Spinner added with conditions

--- news-app/src/components/News.js
+++ news-app/src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItems from './NewsItems'
+import Spinner from './Spinner'
 
 export class News extends Component {
   constructor() {
@@ -13,61 +14,67 @@ export class News extends Component {
   }
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2adb1baa059e40e4adb0d5b8e4795953&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    this.setState({ loading: true })
     let data = await fetch(url)
     let parsedData = await data.json()
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     })
-    console.log('News Data======', data)
   }
   handlePrevClick = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2adb1baa059e40e4adb0d5b8e4795953&page=${this.state.page}&pageSize=${this.props.pageSize}`
+    this.setState({ loading: true })
     let data = await fetch(url)
     let parsedData = await data.json()
     this.setState({
       articles: parsedData.articles,
       page: this.state.page - 1,
+      loading: false,
     })
-    console.log('News Data======', data)
   }
   handleNextClick = async () => {
     if (
-      this.state.page + 1 >
-      Math.ceil(this.state.totalResults / this.props.pageSize)
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
     ) {
-    } else {
       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2adb1baa059e40e4adb0d5b8e4795953&page=${this.state.page}&pageSize=${this.props.pageSize}`
+      this.setState({ loading: true })
       let data = await fetch(url)
       let parsedData = await data.json()
       this.setState({
         articles: parsedData.articles,
         page: this.state.page + 1,
+        loading: false,
       })
-      console.log('News Data======', data)
     }
   }
   render() {
     return (
       <div className="container my-3">
         <h1 className="text-center">News Application</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItems
-                  title={element.title ? element.title.slice(0, 45) : ''}
-                  description={
-                    element.description
-                      ? element.description.slice(0, 88)
-                      : null
-                  }
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            )
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItems
+                    title={element.title ? element.title.slice(0, 45) : ''}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : null
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              )
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
