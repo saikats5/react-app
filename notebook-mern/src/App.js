30ea64df7a61f44bfc75006ba5135cbe3cf42880
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-23 17:01:36 +0530

    UI basic structure setup

--- notebook-mern/src/App.js
+++ notebook-mern/src/App.js
@@ -1,7 +1,21 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Navbar from './components/Navbar'
 
 function App() {
-  return <div className="App">Hi</div>
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App
