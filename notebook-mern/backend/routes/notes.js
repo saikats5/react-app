d47539f677dbd6229270fbb75c4a915701e0d220
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-23 13:28:16 +0530

    Put api updated

--- notebook-mern/backend/routes/notes.js
+++ notebook-mern/backend/routes/notes.js
@@ -49,4 +49,32 @@ router.post(
   }
 )
 
+//Route 3: Update an existing note using PUT "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body
+  const newNote = {}
+  if (title) {
+    newNote.title = title
+  }
+  if (description) {
+    newNote.description = description
+  }
+  if (tag) {
+    newNote.tag = tag
+  }
+  let note = await Note.findById(req.params.id)
+  if (!note) {
+    return res.status(404).send('Not found!!!')
+  }
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send('Not Allowed!!!')
+  }
+  note = await Note.findByIdAndUpdate(
+    req.params.id,
+    { $set: newNote },
+    { new: true }
+  )
+  res.json({ note })
+})
+
 module.exports = router
