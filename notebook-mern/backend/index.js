60bd79e4adef58f71e0cc9d65ba162fb8395cfba
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-24 16:14:56 +0530

    Home component setup

--- notebook-mern/backend/index.js
+++ notebook-mern/backend/index.js
@@ -18,5 +18,5 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Notebook application listening on port ${port}`)
 })
