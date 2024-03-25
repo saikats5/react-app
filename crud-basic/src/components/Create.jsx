701b6e6eb7b492767e264fa4cb1c76931b640c6e
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-25 23:07:09 +0530

    Bootstrap styling added for create and home page

--- crud-basic/src/components/Create.jsx
+++ crud-basic/src/components/Create.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Create = () => {
-  return <div>Create</div>
+  return (
+    <div className="d-flex w-100 vh-100 justify-content-center align-items-center bg-light">
+      <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
+        <h1>Add a User</h1>
+        <form>
+          <div className="mb-2">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Enter Name"
+            ></input>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Enter Email"
+            ></input>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="phone">Phone:</label>
+            <input
+              type="text"
+              name="phone"
+              className="form-control"
+              placeholder="Enter Phone"
+            ></input>
+          </div>
+          <button className="btn btn-success">Submit</button>
+          <Link to="/" className="btn btn-primary ms-3">
+            Back
+          </Link>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Create
--- crud-basic/src/components/Home.jsx
+++ crud-basic/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   const [data, setData] = useState([])
@@ -14,7 +15,45 @@ const Home = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100">
       <h1>List of Users</h1>
-      <div className="w-75 rounded bg-white border shadow p-4"></div>
+      <div className="w-75 rounded bg-white border shadow p-4">
+        <div className="d-flex justify-content-end">
+          <Link to="/create" className="btn btn-success">
+            Add +
+          </Link>
+        </div>
+        <table className="table table-stipend">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, ind) => {
+              return (
+                <tr key={ind}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                  <td>
+                    <button className="btn btn-sm btn-info me-2">Read</button>
+                    <button className="btn btn-sm btn-primary me-2">
+                      Edit
+                    </button>
+                    <button className="btn btn-sm btn-danger me-2">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
