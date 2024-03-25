258ec29d4f1ab079c787eb512db83727c6b21211
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-25 22:45:57 +0530

    basic data setup

--- crud-basic/src/components/Home.jsx
+++ crud-basic/src/components/Home.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Home = () => {
-  return <div>Home</div>
+  const [data, setData] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      let response = await axios.get('http://localhost:3000/users')
+      response = await response.data
+      setData(response)
+    }
+    fetchData()
+  }, [])
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100">
+      <h1>List of Users</h1>
+      <div className="w-75 rounded bg-white border shadow p-4"></div>
+    </div>
+  )
 }
 
 export default Home
