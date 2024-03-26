d90cdec15ac0ce0c190dacbb95f1d51009be5edc
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-26 11:58:12 +0530

    initial redux stor setup

---
+++ ecommerce-thunk/src/redux/reducers/productReducer.js
@@ -0,0 +1,17 @@
+import { ActionTypes } from '../constants/action-types'
+const initialState = {
+  products: [
+    {
+      name: 'Saikat Saha',
+    },
+  ],
+}
+
+export const productReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state
+    default:
+      return state
+  }
+}
