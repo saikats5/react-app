478736a79e829375fec1118374aa0e2421fd1b2c
Saikat Saha <saikats_5@yahoo.co.in>
2024-03-30 17:26:59 +0530

    cart application using toolkit

---
+++ cart-toolkit/src/App.js
@@ -0,0 +1,29 @@
+import './App.css'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import Navbar from './components/Navbar'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+import NotFound from './components/NotFound'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/cart" element={<Cart />}></Route>
+          <Route exact path="/not-found" element={<NotFound />}></Route>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="*" element={<Navigate to="/not-found" />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
