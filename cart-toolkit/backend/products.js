ab2c7faba85353cc2da91ce0473e8cda9432b356
Saikat Saha <saikats_5@yahoo.co.in>
2024-04-01 10:51:20 +0530

    store creation

---
+++ cart-toolkit/backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    fruit: 'Apple',
+    price: 35,
+    image: './public/apple.avif',
+  },
+  {
+    id: 2,
+    fruit: 'Banana',
+    price: 20,
+    image: './public/banana.jpeg',
+  },
+  {
+    id: 3,
+    fruit: 'Melons',
+    price: 50,
+    image: './public/melon.jpeg',
+  },
+  {
+    id: 4,
+    fruit: 'Apple',
+    price: 15,
+    image: './public/lime.jpeg',
+  },
+]
+
+module.exports = products
