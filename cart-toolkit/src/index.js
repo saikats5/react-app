80772bb655d72dc4c783475ebec5a77ed2ab91ea
Saikat Saha <saikats_5@yahoo.co.in>
2024-04-06 10:35:40 +0530

    middleware setup

--- cart-toolkit/src/index.js
+++ cart-toolkit/src/index.js
@@ -4,10 +4,15 @@ import App from './App'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import productsReducer, { productsFetch } from './features/productsSlice'
+import { productsApi } from './features/productsApi'
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware().concat(productsApi.middleware)
   },
 })
 
