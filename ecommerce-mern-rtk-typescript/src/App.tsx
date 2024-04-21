02e188da9809bc9fd0f1a861df47fc85dcc66be6
Saikat Saha <saikats_5@yahoo.co.in>
2024-04-21 19:16:25 +0530

    Suspense implemented for component fallback

--- ecommerce-mern-rtk-typescript/src/App.tsx
+++ ecommerce-mern-rtk-typescript/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
+import Loader from './components/Loader'
 
 const Home = lazy(() => import('./pages/Home'))
 const Search = lazy(() => import('./pages/Search'))
@@ -8,11 +9,13 @@ const Cart = lazy(() => import('./pages/Cart'))
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
