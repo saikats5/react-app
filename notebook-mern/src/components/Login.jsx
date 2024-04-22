93ec7fd8ea224a7410d305c6ab2643b64f58894a
Saikat Saha <saikats_5@yahoo.co.in>
2024-04-22 09:44:56 +0530

    login setup

--- notebook-mern/src/components/Login.jsx
+++ notebook-mern/src/components/Login.jsx
@@ -1,7 +1,52 @@
 import React from 'react'
 
 const Login = () => {
-  return <div>Login</div>
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch('http://localhost:4000/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const json = await response.json()
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            id="password"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default Login
