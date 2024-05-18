efdc229b9e211ba5695d9c43e59f74f3032a79e6
Saikat Saha <saikats_5@yahoo.co.in>
2024-05-18 19:09:20 +0530

    note changes

--- react-typescript/src/components/add-note/add-note.tsx
+++ react-typescript/src/components/add-note/add-note.tsx
@@ -1,10 +1,10 @@
 import { NoteType } from '../note/note-type'
 import './add-note.css'
 import { useState } from 'react'
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 type AddNoteProps = {
-  addNote = (note: NoteType) => void
+  addNote: (note: NoteType) => void
 }
 
 const AddNote = (props: AddNoteProps) => {
@@ -12,17 +12,26 @@ const AddNote = (props: AddNoteProps) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value)
   }
-  const handleClick = () => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
     props.addNote({
       text,
       priority: 'low',
-      id: uuidv4
+      id: uuidv4(),
     })
   }
+  // const handleSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   console.log()
+  // }
   return (
     <div>
       <form className="add-note">
-        <input type="text" onChange={(e) => handleChange} />
+        <input type="text" onChange={handleChange} />
+        {/* <select onChange={(e) => handleSelect}>
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
+        </select> */}
         <button onClick={handleClick}>Add</button>
       </form>
     </div>
--- react-typescript/src/components/note/data.ts
+++ react-typescript/src/components/note/data.ts
@@ -2,32 +2,32 @@ import { NoteType } from './note-type'
 
 export const NotesData: NoteType[] = [
   {
-    id: 1,
+    id: '1',
     text: 'This is note 1',
     priority: 'high',
   },
   {
-    id: 2,
+    id: '2',
     text: 'This is note 2',
     priority: 'low',
   },
   {
-    id: 3,
+    id: '3',
     text: 'This is note 3',
     priority: 'medium',
   },
   {
-    id: 4,
+    id: '4',
     text: 'This is note 4',
     priority: 'low',
   },
   {
-    id: 5,
+    id: '5',
     text: 'This is note 5',
     priority: 'medium',
   },
   {
-    id: 6,
+    id: '6',
     text: 'This is note 6',
     priority: 'high',
   },
